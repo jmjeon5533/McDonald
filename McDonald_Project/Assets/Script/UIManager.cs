6d1850a4bd4aa62d84ad35dd6142dce407be7cb6
jmjeon5533 <jmjeon5533@gmail.com>
2023-06-13 20:30:02 +0900

    Update

--- McDonald_Project/Assets/Script/UIManager.cs
+++ McDonald_Project/Assets/Script/UIManager.cs
@@ -8,11 +8,17 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager instance { get; private set; }
     public Text ScoreText;
+    [SerializeField] Image HPBar;
     public List<Transform> WeaponUI = new List<Transform>();
     public int Score = 0;
+    [SerializeField] FirstPersonController player;
     private void Awake()
     {
         instance = this;
+    }
+    private void Update()
+    {
+        HPBar.fillAmount = player.HP / player.MaxHp;
     }
     public void InitWeaponUI(int Index)
     {
