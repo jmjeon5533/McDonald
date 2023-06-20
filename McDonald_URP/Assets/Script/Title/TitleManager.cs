735d4c0df3fb9b2413466c956b01e129ad5456f4
jmjeon5533 <jmjeon5533@gmail.com>
2023-06-20 17:17:42 +0900

    URP Update

---
+++ McDonald_URP/Assets/Script/Title/TitleManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] Transform[] CamPos;
+    [SerializeField] Transform CamArm;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
